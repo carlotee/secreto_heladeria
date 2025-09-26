c8176d1da3e3db68088afeec31f188312d63caee
carlotee <carlosvega3031@gmail.com>
2025-09-26 02:05:48 -0300

    se actualiza el urls de secreto_heladeria

--- secreto_heladeria/urls.py
+++ secreto_heladeria/urls.py
@@ -15,17 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.contrib import admin
 from django.urls import path, include   # importa bien include
-from organizations import views   
-from organizations import views
-from organizations.views import dashboard
-from django.contrib import admin
-from django.urls import path, include
-
-
+from accounts import views as accounts_views
+from organizations import views as org_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.dashboard, name="dashboard"),    
+    path("dashboard/", org_views.dashboard, name="dashboard"),   
+    path('registro/', accounts_views.registro, name='registro'),
+    path('login/', accounts_views.login_view, name='login'),
 ]
