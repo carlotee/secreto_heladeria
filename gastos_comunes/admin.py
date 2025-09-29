8b7761b0f5d52ad8ffee0f4f2abcf3f1d50203af
carlotee <carlosvega3031@gmail.com>
2025-09-29 20:32:16 -0300

    se actualiza el proyecto

--- gastos_comunes/admin.py
+++ gastos_comunes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import GastosComunes, BoletaGC
 
-# Register your models here.
+
+class GastosComunesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('nombre',)
+    ordering = ('id',)
+
+
+class BoletaGCAdmin(admin.ModelAdmin):
+    list_display = ('id', 'valor', 'fecha', 'gastosComunes')
+    search_fields = ('valor',)
+    list_filter = ('fecha', 'gastosComunes')
+    ordering = ('-fecha',)
+
+
+# Registro manual (sin duplicación)
+admin.site.register(GastosComunes, GastosComunesAdmin)
+admin.site.register(BoletaGC, BoletaGCAdmin)
--- organizations/admin.py
+++ organizations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Organization
 
-# Register your models here.
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at', 'deleted_at')
+    search_fields = ('name',)
+    list_filter = ('created_at', 'updated_at', 'deleted_at')
+    ordering = ('id',)
