5c11b7348abe781ce06562f3cc6b601a782528b2
Carlos Vega <carlosvega3031@gmail.com>
2025-10-06 17:46:25 -0300

    se elimina organizations

--- organizations/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Organization
-
-@admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'created_at', 'updated_at', 'deleted_at')
-    search_fields = ('name',)
-    list_filter = ('created_at', 'updated_at', 'deleted_at')
-    ordering = ('id',)
