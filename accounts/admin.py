aeb087d506b8927b099c46a17cd1721503c6b838
Carlos Vega <carlosvega3031@gmail.com>
2025-10-28 11:51:56 -0300

    se modifica el admin y el models de accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,9 +4,23 @@ from .models import Usuario, Registro
 
 @admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
-    list_display = ('username', 'email', 'numero', 'is_staff', 'is_superuser', 'is_active')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    list_display = ('username', 'email', 'rol', 'is_active', 'is_staff')
+    list_filter = ('rol', 'is_active', 'is_staff')
     search_fields = ('username', 'email')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Rol y número telefónico', {
+            'fields': ('rol', 'numero',),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Rol y número telefónico', {
+            'classes': ('wide',),
+            'fields': ('rol', 'numero'),
+        }),
+    )
+
 
 @admin.register(Registro)
 class RegistroAdmin(admin.ModelAdmin):
--- accounts/models.py
+++ accounts/models.py
@@ -2,18 +2,24 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
+    ROLES = [
+        ('admin', 'Administrador'),
+        ('proveedor', 'Proveedor'),
+        ('cliente', 'Cliente'),
+    ]
+
     username = models.CharField(max_length=191, unique=True)
     email = models.EmailField(unique=True, max_length=191)
     numero = models.CharField(max_length=15, null=True, blank=True)
+    rol = models.CharField(max_length=20, choices=ROLES, default='cliente')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.username} ({self.email})"
+        return f"{self.username} ({self.get_rol_display()})"
     
-    class Meta:  # 👈 Esto DEBE ir dentro del modelo
+    class Meta:
         db_table = 'login'
-
 
 class Registro(models.Model):
     usuario = models.CharField(max_length=50, db_column='usuario')
